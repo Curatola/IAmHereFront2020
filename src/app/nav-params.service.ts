b22d2bf4d9d7541978359ba40b10a51427eaca1e
joel-willb@hotmail.com <joel-willb@hotmail.com>
2019-05-23 13:56:36 -0300

    NavParams

---
+++ src/app/nav-params.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavParamsService {
+  private params: {[key: string]: any};
+  constructor() { }
+
+  public setParams(params: {[key: string]: any}): void{
+    this.params = params;
+  }
+
+  public get(key: string): any{
+    return this.params[key]
+  }
+}
