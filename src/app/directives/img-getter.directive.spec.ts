2e4fa2cc09aa190d1c66a12cc84e503226f469de
Curatola <olavocuratola@gmail.com>
2019-08-22 12:34:23 -0300

    Directive Test

--- src/app/directives/img-getter.directive.spec.ts
+++ src/app/directives/img-getter.directive.spec.ts
@@ -1,8 +1,14 @@
 import { ImgGetterDirective } from './img-getter.directive';
+import { Platform, ActionSheetController } from '@ionic/angular';
+import { CameraService } from '../service/camera.service';
 
 describe('ImgGetterDirective', () => {
+  let plat:Platform;
+  let camera:CameraService;
+  let action:ActionSheetController;
+
   it('should create an instance', () => {
-    const directive = new ImgGetterDirective();
+    const directive = new ImgGetterDirective(plat,camera,action);
     expect(directive).toBeTruthy();
   });
 });
